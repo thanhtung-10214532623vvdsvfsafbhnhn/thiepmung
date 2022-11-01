cff6bcef40ea5658468c0a7d3da4cf2fa28a4994
thanhtung-10214532623vvdsvfsafbhnhn <117196789+thanhtung-10214532623vvdsvfsafbhnhn@users.noreply.github.com>
2022-11-01 23:17:31 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Em';
+const giftUrl = '';
+const eventName = 'Happy Birthday 02-11';
+const titleCard = 'Gửi từ nơi xa <i class="fa-solid fa-face-smile-wink"></i>';
+const contentCard = 'Happy Birthday! Chúc "aquinn" sinh nhật đầy ắp yêu thuơng và tiếng cười, thêm tuổi, thêm hạnh phúc, thêm nhiều niềm vui nhé!';
+
+
+const giftImage = 'anhfb.jbg';
+const base64 = '';
+const giftImageBase64 = "data:image/jpg;base64, " + base64;
